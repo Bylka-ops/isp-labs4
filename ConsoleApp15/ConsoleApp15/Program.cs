d14f06df1cadea979473459e41e85194a8a671dd
AmigoElMato <artem.papikyan.00@bk.ru>
2020-04-15 16:30:58 +0300

    first commit

---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly dll = Assembly.Load("ClassLibrary1");
+            Type tp = dll.GetType("ClassLibrary1.BasicInfo");
+            object ob = Activator.CreateInstance(tp);
+            MethodInfo mi;
+            mi = tp.GetMethod("print");
+            mi.Invoke(ob,null);
+           Console.ReadKey();
+        }
+    }
+}
